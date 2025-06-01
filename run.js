5a753931250bee8c03fced26de3a72872404a3c2
Hexel - cloud <hexelclouds@gmail.com>
2025-06-02 03:38:15 +0700

    Create run.js

---
+++ run.js
@@ -0,0 +1,22 @@
+const { spawn } = require('child_process');
+/**
+ * Function to start a command process.
+ * @param {string} cmd - The command to execute.
+ */
+function start(cmd) {
+    try {
+        // Spawn the desired command process
+        const childProcess = spawn(cmd, [], {
+            stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+        });
+
+        // Handle errors
+        childProcess.on('error', (error) => {
+            console.error('Error starting process:', error.message);
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+start('bash');
